10961f01b80babf9cbc4ade66fd4e7e2a803dd2a
Rohit Voleti <rnvoleti@asu.edu>
2018-07-18 19:44:27 -0700

    added recipy functionality to project

--- src/runall-wines.py
+++ src/runall-wines.py
@@ -1,5 +1,6 @@
 import importlib
 import sys
+import recipy
 
 subset = importlib.import_module('.data.01_subset-data-GBP', 'src')
 plotwines = importlib.import_module('.visualization.02_visualize-wines', 'src')
